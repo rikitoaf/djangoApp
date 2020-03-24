d89a3ae557a1fddd04630bba2c0ba8492b04dfe3
AF Rakib <rikitoaf@gmail.com>
2020-03-24 19:18:37 +0600

    profile update option added

--- users/models.py
+++ users/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from PIL import Image
 # Create your models here.
 
 class Profile(models.Model):
@@ -8,4 +9,6 @@ class Profile(models.Model):
 
     def __str__(self):
         return f'{self.user.username} Profile'
+
+        
 
--- users/views.py
+++ users/views.py
@@ -2,25 +2,47 @@ from django.shortcuts import render,redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from .forms import UserRegistrationForm
+from .forms import UserRegisterForm,UserUpdateForm,ProfileUpdateForm
 
 # Create your views here.
 
 def register (request):
 
     if request.method == 'POST':
-        form = UserRegistrationForm (request.POST)
+        form = UserRegisterForm (request.POST)
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
 
-            messages.success(request, f"{username} you succesfully created account!")
-            
+            messages.success(request, f" you succesfully created account!")           
             return redirect('login')
     else:
-        form = UserRegistrationForm()
+        form = UserRegisterForm()
     return render (request, 'users/register.html', {'form': form})
+
 
 @login_required
 def profile (request):
-    return render (request, 'users/profile.html')
+    if request.method == 'POST':
+        u_form = UserUpdateForm(request.POST,instance= request.user)
+        p_form = ProfileUpdateForm(request.POST, request.Files , instance= request.user.profile)
+
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            messages.success(request, f"your account is succesfully Updated!")           
+            return redirect('profile')
+        
+
+    else:
+        u_form = UserUpdateForm(instance= request.user)
+        p_form = ProfileUpdateForm(instance= request.user.profile)
+
+    context = {
+        'u_form': u_form,
+        'p_form': p_form,
+    }
+    return render (request, 'users/profile.html',context)
+
+
+#issues uploading default pic
