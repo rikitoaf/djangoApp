28e90046ec8976644c9ca38e0479c15bd9a03faf
AF Rakib <rikitoaf@gmail.com>
2020-03-25 13:20:18 +0600

    update and delete addded

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
+from .views import PostListView,PostDetailView,PostCreateView , PostUpdateView , PostDeleteView
 from . import views
 
 urlpatterns = [
-    path('',views.home, name = 'blog-home'),
+    
+    path('',views.PostListView.as_view(), name = 'blog-home'),
+    path('post/new/',views.PostCreateView.as_view(), name = 'post-create'),
+    path('post/<int:pk>/',views.PostDetailView.as_view(), name = 'post-detail'),
+    path('post/<int:pk>/update/',views.PostUpdateView.as_view(), name = 'post-update'),
+     path('post/<int:pk>/delete/',views.PostDeleteView.as_view(), name = 'post-delete'),
     path('about/',views.about, name = 'blog-about'),
 
 ]
